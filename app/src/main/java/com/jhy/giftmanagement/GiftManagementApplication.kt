1b7a8c92e9814a42cce9687fae42cac9f7889d5d
gkduduu <gkduduu@naver.com>
2023-03-29 14:46:34 +0900

    Koltin버전 변경 및 hilt추가

--- app/src/main/java/com/jhy/giftmanagement/GiftManagementApplication.kt
+++ app/src/main/java/com/jhy/giftmanagement/GiftManagementApplication.kt
@@ -1,7 +1,7 @@
 package com.jhy.giftmanagement
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GiftManagementApplication {
-}
+class GiftManagementApplication : Application()
